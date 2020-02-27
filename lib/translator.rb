784b48d7e300034cea539d3fd52d017e8c78a7b6
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-27 04:16:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,8 @@ def load_library(path)
   hash[:get_meaning] = {}
   hash[:get_emoticon] = {}
   emoticons.each do |key,value|
-    hash[:get_meaning] = value
-    hash[:get_emoticon] = value
+    hash[:get_meaning][value][1] = key
+    hash[:get_emoticon][value][0] = value[1]
   end
   pp hash
 end
