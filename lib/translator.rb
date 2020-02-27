9ced9a9563bbd54ffa043fa2e5917836ab39eccb
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-27 04:00:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,13 @@ require "yaml"
 def load_library(path)
   emoticons = YAML.load_file(path)
   pp emoticons
+  emoticons[get_meaning] = []
+  emoticons[get_emoticon] = []
+  emoticons.each do |key,value|
+    emoticons[get_meaning] << value
+    emoticons[get_emoticon] << value
+  end
+
 end
 
 def get_japanese_emoticon
